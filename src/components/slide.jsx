5b7c9ca24dc43915b2fff5ff6f81d0bc6961a096
LeVuWan <levuwan@gmail.com>
2024-09-14 13:43:28 +0700

    v4

--- src/components/slide.jsx
+++ src/components/slide.jsx
@@ -1,29 +1,53 @@
-import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  AtSignIcon,
+  CalendarIcon,
+  EditIcon,
+  HamburgerIcon,
+} from "@chakra-ui/icons";
 import { List, ListIcon, ListItem, useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import { BiMenu } from "react-icons/bi";
 import { NavLink } from "react-router-dom";
 
+const defaultIconSize = "1.875rem";
+
 const Slide = () => {
+  const [isNavIcon, setIsNavIcon] = useState(false);
   return (
-    <List color="white" fontSize="1.2em" spacing={4}>
-      <ListItem>
-        <NavLink to="/">
-          <ListIcon as={CalendarIcon} color="white" />
-          Dashboard
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to="/create">
-          <ListIcon as={EditIcon} color="white" />
-          New Task
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to="/profile">
-          <ListIcon as={AtSignIcon} color="white" />
-          Profile
-        </NavLink>
-      </ListItem>
-    </List>
+    <>
+      <BiMenu
+        className="cursor-pointer md:hidden "
+        size={defaultIconSize}
+        onClick={() => {
+          setIsNavIcon(!isNavIcon);
+        }}
+      />
+      <List
+        color="white"
+        fontSize="1.2em"
+        spacing={4}
+        className={` mx-4 my-2${isNavIcon ? "" : " hidden"} md:block`}
+      >
+        <ListItem>
+          <NavLink to="/">
+            <ListIcon as={CalendarIcon} color="white" />
+            Dashboard
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/create">
+            <ListIcon as={EditIcon} color="white" />
+            New Task
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/profile">
+            <ListIcon as={AtSignIcon} color="white" />
+            Profile
+          </NavLink>
+        </ListItem>
+      </List>
+    </>
   );
 };
 
