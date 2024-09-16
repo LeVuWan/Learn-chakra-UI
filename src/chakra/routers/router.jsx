0357e5ae2d58309df9e29bcb5baa0c0002b6da76
LeVuWan <levuwan@gmail.com>
2024-09-16 16:25:25 +0700

    v11

--- src/chakra/routers/router.jsx
+++ src/chakra/routers/router.jsx
@@ -7,18 +7,18 @@ import {
 } from "react-router-dom";
 
 // layouts and pages
-import Dashboard, { tasksLoader } from "../pages/Dashboard";
 import Create from "../pages/Create";
 import Profile from "../pages/Profile";
 import RootLayout from "../layouts/RootLayout";
 import App from "../../tailwind/taiwind";
+import Dashboard from "../pages/Dashboard";
 
 // router and routes
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<RootLayout />}>
-        <Route index element={<Dashboard />} loader={tasksLoader} />
+        <Route index element={<Dashboard />} />
         <Route path="create" element={<Create />} />
         <Route path="profile" element={<Profile />} />
       </Route>
