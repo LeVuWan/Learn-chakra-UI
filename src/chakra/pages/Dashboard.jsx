be52f40c58b48f566fca0ab58fce13e5f5b54eab
LeVuWan <levuwan@gmail.com>
2024-09-16 16:14:30 +0700

    v9

--- src/chakra/pages/Dashboard.jsx
+++ src/chakra/pages/Dashboard.jsx
@@ -241,12 +241,3 @@ export default function Dashboard() {
     </SimpleGrid>
   );
 }
-
-export const tasksLoader = async () => {
-  const res = await fetch("http://localhost:3000/tasks");
-  if (!res.ok) {
-    throw new Error("Failed to fetch tasks");
-  }
-  const data = await res.json();
-  return data;
-};
